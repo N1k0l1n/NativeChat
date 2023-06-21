6549978e8f1db7b411e2327869516e660b744df3
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-06-21 15:15:35 +0200

    animated splash screen

--- App.js
+++ App.js
@@ -5,34 +5,38 @@ import ChatScreen from "./screens/ChatScreen";
 import MessageScreen from "./screens/MessageScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import GlobalState from "./context";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <GlobalState>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="ChatScreen"
-            component={ChatScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="MessageScreen"
-            component={MessageScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar hidden={true} />
-    </GlobalState>
+    <SafeAreaProvider>
+      <GlobalState>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="ChatScreen"
+              component={ChatScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MessageScreen"
+              component={MessageScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar hidden={true} />
+      </GlobalState>
+    </SafeAreaProvider>
   );
 }
 
